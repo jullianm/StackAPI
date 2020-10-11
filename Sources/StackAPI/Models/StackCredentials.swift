03dc49e5209bcbdce8ff5f3925f57400393b2d78
Jullian <jllnmercier@gmail.com>
2020-10-11 18:14:22 -0400

    add init from decoder

--- Sources/StackAPI/Models/StackCredentials.swift
+++ Sources/StackAPI/Models/StackCredentials.swift
@@ -11,4 +11,15 @@ public struct StackCredentials: Decodable {
     public let clientId: String
     public let key: String
     public var token: String = .init()
+    
+    enum CodingKeys: String, CodingKey {
+        case clientId
+        case key
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        clientId = try container.decode(String.self, forKey: .clientId)
+        key = try container.decode(String.self, forKey: .key)
+    }
 }
